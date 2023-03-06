8f6c8457951b6538c6e415c5879de0bd5efbef05
pham duy <phd051199@gmail.com>
2023-03-06 18:49:50 +0700

    test

--- src/commands/chatgpt.ts
+++ src/commands/chatgpt.ts
@@ -1,16 +1,18 @@
-import { Client } from 'discord.js';
+import { Client, Events } from 'discord.js';
 
 export const gpt3 = (client: Client): void => {
-  client.on('messageCreate', async (message) => {
+  client.on(Events.MessageCreate, async (message) => {
     const mess = message.content.split(' ');
     if (mess[0].toLowerCase() === '!gpt') {
       try {
-        const data = await global.openai.sendMessage(
-          message.content.replace('!gpt', ''),
-        );
-        message.reply(data.text);
+        const msgRef = await message.reply('...');
+        await global.openai.sendMessage(message.content.replace('!gpt', ''), {
+          onProgress: async (partialResponse: { text: string }) => {
+            await msgRef.edit(partialResponse.text);
+          },
+        });
       } catch (e) {
-        message.reply('Fail');
+        await message.reply('Fail');
       }
     }
   });
